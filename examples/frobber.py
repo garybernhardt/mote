55fbaaee4fa4668c4bbd4e6c043162270464c52e
Gary Bernhardt <gary.bernhardt@gmail.com>
2008-12-08 18:53:37 -0500

    Added an example and a system test that runs it

---
+++ examples/frobber.py
@@ -0,0 +1,51 @@
+from nose.tools import assert_raises
+
+
+# Class under test
+class Frobber:
+    def __init__(self):
+        self.ready_to_frob = True
+    def frazzle(self):
+        self.ready_to_frob = False
+    def frob(self):
+        self.spam = 5
+
+
+# Specs (tests)
+def describe_frobber():
+    frobber = Frobber()
+
+    def should_be_ready_to_frob():
+        assert frobber.ready_to_frob
+
+    def should_not_have_spam():
+        assert_raises(AttributeError, lambda: frobber.spam)
+
+    def when_frazzled():
+        frobber.frazzle()
+
+        def should_not_be_ready_to_frob():
+            assert not frobber.ready_to_frob
+
+        def should_be_ready_to_frob(): # FAILS - contradicts previous test
+            assert frobber.ready_to_frob
+
+    def when_frobbed():
+        frobber.frob()
+
+        def should_have_spam():
+            assert frobber.spam
+
+
+# Output:
+#
+# describe frobber
+#   should be ready to frob -> ok
+#   should not have spam -> ok
+#   when frazzled
+#     should not be ready to frob -> ok
+#     should be ready to frob -> FAIL (AssertionError @ 31)
+#   when frobbed
+#     should have spam -> ok
+# All specs passed
+
---
+++ tests/system/test_examples.py
@@ -0,0 +1,28 @@
+import os
+
+
+from tests.system.test_mote import SystemTest
+
+
+class TestExamples(SystemTest):
+    def assert_output_matches_comments(self, path):
+        file_contents = [line.strip() for line in file(path, 'r')]
+        output_marker = file_contents.index('# Output:')
+        output_lines = file_contents[output_marker + 2:]
+        output_lines = [line[2:] for line in output_lines]
+        output = '\n'.join(output_lines)
+        assert self._run_mote(path) == output
+
+    def should_match_output_in_example_comments(self):
+        base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '..',
+                                                '..'))
+        examples_dir = os.path.join(base_dir, 'examples')
+        example_file_paths = [path
+                              for path in os.listdir(examples_dir)
+                              if path.endswith('.py')]
+
+        for path in example_file_paths:
+            abs_path = os.path.join(examples_dir, path)
+            yield self.assert_output_matches_comments, abs_path
+
