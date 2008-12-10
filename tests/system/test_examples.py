9240a99de19499e9afa3afb4747557e74c366e2a
Gary Bernhardt <gary.bernhardt@gmail.com>
2008-12-10 16:02:42 -0500

    Made example test failures print the expected and recieved output

--- tests/system/test_examples.py
+++ tests/system/test_examples.py
@@ -6,12 +6,13 @@ from tests.system.test_mote import SystemTest
 
 class TestExamples(SystemTest):
     def assert_output_matches_comments(self, path):
+        self.test_file_path = path
         file_contents = [line.strip() for line in file(path, 'r')]
         output_marker = file_contents.index('# Output:')
         output_lines = file_contents[output_marker + 2:]
         output_lines = [line[2:] for line in output_lines]
         output = '\n'.join(output_lines)
-        assert self._run_mote(path) == output
+        self._assert_output_equals(output)
 
     def should_match_output_in_example_comments(self):
         base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
