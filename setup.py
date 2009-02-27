6960b573d9d2735cac3eab97cabb7fd2b8d6a6ac
Gary Bernhardt <gary.bernhardt@gmail.com>
2009-02-27 13:12:19 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='mote',
+      version='0.1',
+      description='Mote',
+      author='Gary Bernhardt',
+      author_email='gary.bernhardt@gmail.com',
+      packages=find_packages(),
+      license='BSD',
+     )
