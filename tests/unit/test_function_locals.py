348549407828116f6acec35bbaa88ca573e3a739
Gary Bernhardt <gary.bernhardt@gmail.com>
2009-03-29 21:50:29 -0400

    Changed module under test name to "mod" in function locals test

--- tests/unit/test_function_locals.py
+++ tests/unit/test_function_locals.py
@@ -1,22 +1,21 @@
 import sys
 
 from dingus import Dingus, DingusTestCase
-from mote import localfunctions
-from mote.localfunctions import FunctionLocals
+from mote import localfunctions as mod
 
 
-BaseFixture = DingusTestCase(FunctionLocals)
+BaseFixture = DingusTestCase(mod.FunctionLocals)
 
 
 class WhenExaminingFunctionWithALocalFunction(BaseFixture):
     def setup(self):
         super(WhenExaminingFunctionWithALocalFunction, self).setup()
-        localfunctions.sys = sys
+        mod.sys = sys
         def function():
             def local_function():
                 return 'local function return'
 
-        self.function_locals = FunctionLocals(function)
+        self.function_locals = mod.FunctionLocals(function)
 
     def should_have_correct_number_of_local_function(self):
         assert len(self.function_locals) == 1
