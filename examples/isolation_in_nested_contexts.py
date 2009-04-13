499343d0c1d9891f11326ebab347ff691182a592
Gary Bernhardt <gary.bernhardt@gmail.com>
2009-04-12 21:53:19 -0400

    Added a better example/test for nested context and test case isolation

--- examples/isolation_in_nested_contexts.py
+++ examples/isolation_in_nested_contexts.py
@@ -1,14 +1,31 @@
 import itertools
 
 def describe_foo():
-    iterator = itertools.count()
+    calls = []
     def describe_nested():
+        calls.append(1)
+        assert calls == [1]
         def describe_nested_more():
+            calls.append(2)
+            assert calls == [1, 2]
             def describe_nested_even_more():
-                def should_have_a_fresh_iterator_the_first_time():
-                    assert iterator.next() == 0
-                def should_have_a_fresh_iterator_the_second_time():
-                    assert iterator.next() == 0
+                calls.append(3)
+                assert calls == [1, 2, 3]
+                def should_have_fresh_iterator_the_first_time():
+                    calls.append(4)
+                    assert calls == [1, 2, 3, 4]
+                def should_have_fresh_iterator_the_second_time():
+                    calls.append(5)
+                    assert calls == [1, 2, 3, 5]
+        def describe_sibling_context_with_child():
+            calls.append(6)
+            assert calls == [1, 6]
+            def describe_child_of_sibling():
+                calls.append(7)
+                assert calls == [1, 6, 7]
+                def should_have_fresh_iterator():
+                    calls.append(8)
+                    assert calls == [1, 6, 7, 8]
 
 
 # Output:
@@ -17,7 +34,10 @@ def describe_foo():
 #   describe nested
 #     describe nested more
 #       describe nested even more
-#         - should have a fresh iterator the first time
-#         - should have a fresh iterator the second time
+#         - should have fresh iterator the first time
+#         - should have fresh iterator the second time
+#     describe sibling context with child
+#       describe child of sibling
+#         - should have fresh iterator
 # All specs passed
 
