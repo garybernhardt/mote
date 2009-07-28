f5ba538d47e138805699ac6a92216a84d23fbc73
Gary Bernhardt <gary.bernhardt@gmail.com>
2009-07-28 15:27:38 -0700

    Cleaned up automatic running of examples as system tests

--- tests/system/test_examples.py
+++ tests/system/test_examples.py
@@ -1,5 +1,6 @@
 import os
 from glob import glob
+import re
 
 
 from tests.system.test_mote import SystemTest
@@ -8,13 +9,18 @@ from tests.system.test_mote import SystemTest
 class TestExamples(SystemTest):
     def assert_output_matches_comments(self, path):
         self.test_file_path = path
-        file_contents = [line.strip() for line in file(path, 'r')]
-        output_marker = file_contents.index('# Output:')
-        output_lines = file_contents[output_marker + 2:]
-        output_lines = [line[2:] for line in output_lines]
-        output = '\n'.join(output_lines)
-        output = output.replace('$ROOT', os.getcwd())
-        self._assert_output_equals(output)
+        contents = [line.strip() for line in file(path, 'r')]
+        expected_output = self._expected_output_from_file_contents(contents)
+        self._assert_output_equals(expected_output)
+
+    def _expected_output_from_file_contents(self, contents):
+        output_marker = contents.index('# Output:')
+        output_lines_with_comments = contents[output_marker + 2:]
+        output_lines = [re.sub('# ?', '', line)
+                        for line in output_lines_with_comments]
+        raw_output = '\n'.join(output_lines)
+        output = raw_output.replace('$ROOT', os.getcwd())
+        return output
 
     def should_match_output_in_example_comments(self):
         base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
